9e4843ad7ac030925e2b977bdcb50e8e2c8436c1
rutuja26112002@gmail.com <rutuja26112002@gmail.com>
2024-07-29 18:02:37 +0530

    a

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,1 +1,380 @@
-#
+
+
+
+
+
+<div class="width100 footer_section_box">
+    <div class="container">
+        <div class="width100 footer_list">
+            <!-- Bidvertiser2075103 -->
+            <div class="footer_content width100">
+                <div class="foorer_head width100">Useful Links</div>
+                <ul>
+                    <li><a href="/pages/about-us/aboutus.html">About Us</a></li>
+                    <li><a href="#">Acadamic </a></li>
+                    <li><a href="#">Carees</a></li>
+                    <li><a href="#">Infrastucture</a></li>
+                    <li><a href="#">Commitee</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+            
+            <div class="footer_content width100">
+                <div class="foorer_head width100">Admissions</div>
+                <ul>
+                   
+                    <li><a href="/pages/about-us/MBA.HTML">MBA</a></li>
+                    <li><a href="/pages/about-us/MCA.HTML">MCA</a></li>
+                </ul>
+            </div>
+            <div class="footer_content footer_content2 width100">
+                <div class="foorer_head width100">Our Location</div>
+                <!-- <ul> <li><a href="#">Admissions </a></li>
+                  <li><a target="_blank"
+                            href="#">Career </a>
+                    </li><li><a href="contact-us/index.html">Contact Us</a></li>
+                    
+                </ul> -->
+            </div>
+          
+        </div>
+        <div class="width100">
+            <div class="top_social_icon">
+                <a target="_blank" href=""><span
+                        class="facebook_icon"></span></a>
+                <a target="_blank" href=""><span
+                        class="instagram_icon"></span></a>
+                <a target="_blank" href=""><span
+                        class="linkedin_icon"></span></a>
+                <a target="_blank" href=""><span class="twitter_icon"></span></a>
+                <a target="_blank" href=""><span
+                        class="youtube_icon"></span></a>
+            </div>
+            <div class="visitor_counter_box">
+                <div class="visitor_counter_innerbox">
+                    <div class="visitors_text">Our Visitors:</div>
+                    <!-- hitwebcounter Code START -->
+                <a href="https://www.hitwebcounter.com" target="_blank">
+                <img src="https://hitwebcounter.com/counter/counter.php?page=12240375&style=0001&nbdigits=6&type=page&initCount=1205" title="Counter Widget" Alt="Visit counter For Websites"   border="0" /></a>  
+                </div>
+            </div>
+        </div>
+
+        <!-- kajsdfk;sadjfkl;asdjfk'asfdd -->
+    </div>
+</div><script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/660d07dca0c6737bd127c797/1hqhedeae';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    
+    </script>
+    <!--End of Tawk.to Script-->
+
+<div class="footer_enquire_apply_box">
+    <script src="assets/js/chat.js"></script> 
+    <!-- <a href="index.html#enquire_now"><img src="assets/images/enquiry_now_btn.png"></a> -->
+   
+    
+        
+        <a href="https://api.whatsapp.com/send?phone=918668464533&amp;text=Hi" target="_blank"><img
+            src="assets/images/whatsapp_btn.png"></a></div>
+            <!--Start of Tawk.to Script-->
+
+
+<!-- ONLOAD-FACE-EFFECTS-START -->
+<link rel="stylesheet" href="assets/js/aos.js" />
+<script src="assets/js/aos.js"></script>
+<script>
+AOS.init({
+    easing: 'ease-in-out-sine'
+});
+</script>
+<!-- ONLOAD-FACE-EFFECTS-END -->
+
+
+<style>
+.indicator {
+    bottom: 50px !important;
+}
+
+#__eechatIcon {
+    bottom: 56px !important;
+}
+</style>
+<!-- home-testimonial script-start -->
+<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+<script src="assets/js/jquery-1.12.4.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+
+<script src="assets/jquery/jquery.validate.min.js"></script>
+<script src="plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+
+
+
+
+<script>
+$('.faculty_news_slider.owl-carousel').owlCarousel({
+    autoplay: true,
+    center: true,
+    loop: true,
+    navigation: false,
+    items: 1
+});
+</script>
+<script>
+$('.faculty_news_slider.owl-carousel').owlCarousel({
+    autoplay: true,
+    center: true,
+    loop: true,
+    navigation: false,
+    items: 1
+});
+</script>
+<script>
+$('.owl-carousel').owlCarousel({
+    autoplay: false,
+    center: true,
+    loop: true,
+    navigation: false,
+    navigationText: ["", ""],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1
+        },
+        1200: {
+            items: 3
+        },
+        1300: {
+            items: 3
+        }
+    }
+});
+</script>
+<!-- home-testimonial script-end --><!-- HOME-BANNER-SLIDER-START -->
+<script>
+$('.owl-carousel_homebanner').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 5000,
+    center: true,
+    loop: true,
+    navigation: true,
+    navigationText: ["", ""],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1
+        },
+        1200: {
+            items: 1
+        },
+        1300: {
+            items: 1
+        }
+    }
+});
+</script>
+<!-- HOME-BANNER-SLIDER-END --><!--  IMPECCABLE PLACEMENTS -START -->
+<script>
+$('.owl-carousel_impeccable').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 1000,
+    center: true,
+    loop: true,
+    navigation: false,
+    navigationText: ["", ""],
+    responsive: {
+        0: {
+            items: 1,
+            loop: true
+        },
+        600: {
+            items: 1
+        },
+        1200: {
+            items: 6
+        },
+        1300: {
+            items: 6
+        }
+    }
+});
+</script>
+<!--  IMPECCABLE PLACEMENTS -END --><!-- MENU-ACTIVE-START -->
+
+<script type="text/javascript">
+jQuery(function($) {
+
+    var path = window.location.href +
+        "/"; // because the 'href' property of the DOM element is the absolute path
+    //alert($('ul a').length);
+    $('.left_panel li a').each(function() {
+        if (this.href === path) {
+            $(this).addClass('active');
+        }
+        //alert(this.href);
+    });
+});
+</script>
+<script type="text/javascript">
+jQuery(function($) {
+
+    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+    //alert($('ul a').length);
+    $('.left_panel li a').each(function() {
+        if (this.href === path) {
+            $(this).addClass('active');
+        }
+        //alert(this.href);
+    });
+});
+</script>
+<!-- MENU-ACTIVE-END -->
+
+<!-- HOME-MENU-ACTIVE-START -->
+
+<script type="text/javascript">
+jQuery(function($) {
+
+    var path = window.location.href +
+        "/"; // because the 'href' property of the DOM element is the absolute path
+    //alert($('ul a').length);
+    $('.top_box1 li a').each(function() {
+        if (this.href === path) {
+            $(this).addClass('active');
+        }
+        //alert(this.href);
+    });
+});
+</script>
+<script type="text/javascript">
+jQuery(function($) {
+
+    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+    //alert($('ul a').length);
+    $('.top_box1 li a').each(function() {
+        if (this.href === path) {
+            $(this).addClass('active');
+        }
+        //alert(this.href);
+    });
+});
+</script>
+<!-- HOME-MENU-ACTIVE-END -->
+
+
+<!-- VERTICAL-MENU-TABBING-START -->
+<script>
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+</script>
+
+<!-- VERTICAL-MENU-TABBING-END -->
+
+
+<!-- LEFT-MENU-TABBING-START -->
+<script id="rendered-js">
+$.noConflict();
+$(document).ready(function() {
+
+    $('ul.tabs li').click(function() {
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#" + tab_id).addClass('current');
+    });
+
+});
+//# sourceURL=pen.js
+</script>
+<!-- LEFT-MENU-TABBING-END -->
+
+
+
+<!-- VERTICAL-MENU-TABBING-START -->
+
+<!-- VERTICAL-MENU-TABBING-END -->
+<script type="text/javascript">
+function getcourse(course_type) {
+    var _token = jQuery('input[name="_token"]').val();
+    jQuery.ajax({
+        url: "",
+        method: "POST",
+        data: {
+            course_type: course_type,
+            _token: _token
+        },
+        success: function(result) {
+            jQuery('#course').html(result);
+        }
+    })
+}
+</script>
+<script type="text/javascript">
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.async = true;
+s.src = "../widgets.nopaperforms.com/emwgts.html";
+document.body.appendChild(s);
+</script><script type="text/javascript">
+// Remove the spaces from the entered and generated code
+function removeSpaces(string) {
+    return string.split(' ').join('');
+}
+</script>
+
+
+
+
+<!-- INDEX-LATEST-NEWS-ANNOUNCEMENT-FOUR-TABBING-START -->
+<script id="rendered-js">
+$(document).ready(function() {
+    $('.home_foru_tab li').click(function() {
+        var tab_id = $(this).attr('data-tab');
+        $('.home_foru_tab li').removeClass('current');
+        $('.four_content_box').removeClass('current');
+        $(this).addClass('current');
+        $("#" + tab_id).addClass('current');
+    });
+});
+</script>
+<style>
+.four_content_box {
+    display: none;
+}
+
+.four_content_box.current {
+    display: block;
+}
+</style>
+</body>
+</html>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="google-site-verification" content=" "/>
+    <meta name="google-site-verification" content="" />
+    <title>Home-PCIMCA</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="article:publisher" content="" />
+    <meta property="article:modified_time" content="" />
+    <meta property="og:image" content="" />
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/font-awesome/6.1.0/css/all.min.css" />
+    <script src="assets/js/sweetalert.min.js"></script>
+    <script src="assets/js/jquery-3.5.0.min.js"></script>
+    
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','');</script>
+    <!-- End Google Tag Manager -->
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+    
+    <style>
+    body {
+        margin: 0px;
+        padding: 0px;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 13px;
+    }
+    </style>
+
+    <!-- RESPONSIVE-NAV-START -->
+    <script>
+    function myFunction_career() {
+        var x = document.getElementById("career_topmenu");
+        if (x.className === "career_topmenu") {
+            x.className += " responsive_career";
+        } else {
+            x.className = "career_topmenu";
+        }
+    }
+
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "navigation_box") {
+            x.className += " responsive";
+        } else {
+            x.className = "navigation_box";
+        }
+    }
+
+    function myFunction_academics() {
+        var x = document.getElementById("academics_dropdown");
+        if (x.className === "navigation_dropdown_box") {
+            x.className += " academics_open";
+        } else {
+            x.className = "navigation_dropdown_box";
+        }
+    }
+
+    function myFunction_admissions() {
+        var x = document.getElementById("admissions_dropdown");
+        if (x.className === "navigation_dropdown_box admission_dropdown_box") {
+            x.className += " academics_open";
+        } else {
+            x.className = "navigation_dropdown_box admission_dropdown_box";
+        }
+    }
+
+    function myFunction_lounch() {
+        var x = document.getElementById("lounch_mainbox");
+        if (x.className === "width100 lounch_mainbox") {
+            x.className += " lounch_open";
+        } else {
+            x.className = "navigation_dropdown_box admission_dropdown_box";
+        }
+    }
+    </script>
+    <!-- RESPONSIVE-NAV-END -->
+</head>
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- header section start -->
+    <div class="header width100 inner_header_mainbox">
+    <div class="width100 header_background">
+        <div class="width1240">
+            <div class="width100 top_box1">
+                <div class="width100">
+                    <div class="Logo" data-aos="fade-down"><a href="#"> <img src="assets/images/LOGOFINAL.png" width="170"></a></div>
+                    <div class="top_right">
+                        <div class="width100 top_box2">
+                            <div class="top_btn_box">
+                                <a href="# " target="_blank">Login</a>
+                                 
+                            </div>
+                            <div id="career_topmenu" class="career_topmenu">
+                                <a href="javascript:void(0);" class="carrer_menu_toggle menu_toggle" onclick="myFunction_career()"><img src="../pcimca/assets/images/menu_toggle.png"></a>
+                                <ul class="top_menu_box">
+                                    <a href="javascript:void(0);" class="carrer_menu_toggle_close menu_toggle" onclick="myFunction_career()">x</a>
+                                    <li><a href="#">Feedback Form</a>
+                                        <div class="careers_dropdown_box">
+                                            <ul>
+                                                <li><a href="#"><span></span>AICTE Feedback</a></li>
+                                                <li><a target="_blank" href="# ">Grievance Redressal </a></li>
+                                                <li><a target="_blank" href="# ">SC/ST Grievance Redressal</a></li>
+                                                <li><a target="_blank" href="#">Anti Ragging Feedback Form</a></li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    
+                                     <li><a href="#">Merit List</a>
+                                        <div class="careers_dropdown_box">
+                                            <ul>
+                                                <li><a href="/pages/about-us/MBA.HTML"><span></span>MBA</a></li>
+                                                <li><a target="_blank" href="/pages/about-us/MCA.HTML">MCA</a></li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li><a href="#">Infrastucture</a></li>
+                                    <li><a href="#">Mandatory Disclosures</a>
+                                        <div class="careers_dropdown_box">
+                                            <ul>
+                                                <li><a href="#"><span></span>Download Mandatary Report</a></li>
+                                                <li><a target="_blank" href=" ">Download LOA Report</a></li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="width100 top_box3" data-aos="fade-right">
+                            <div class="top_search_box">
+                                <input type="input" placeholder="Search Here...">
+                                <input type="submit" class="submit">
+                            </div>
+                            <div class="top_social_icon">
+                                <a target="_blank" href="https://www.facebook.com "><span class="facebook_icon"></span></a>
+                                <a target="_blank" href="https://www.instagram.com "><span class="instagram_icon"></span></a>
+                                <a target="_blank" href="https://www.linkedin.com "><span class="linkedin_icon"></span></a>
+                                <a target="_blank" href="https://twitter.com "><span class="twitter_icon"></span></a>
+                                <a target="_blank" href="https://www.youtube.com "><span class="youtube_icon"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="width100">
+                    <a href="javascript:void(0);" class="menu_toggle" onclick="myFunction()"><img src="../pcimca/assets/images/menu_toggle.png"></a>
+                    <div id="myTopnav" class="navigation_box">
+                        <a href="javascript:void(0);" class="nav_close_btn" onclick="myFunction()">x</a>
+                        <ul>
+                            <li><a class="selected" href="../../../index.html" style="border-left:0px;"><span></span>Home</a></li>
+                            <li><a href="/pages/about-us/aboutus.html"><span></span>About Us</a></li>
+                            <li><a href="#"><span></span>Acadamic</a></li>
+                            <li>
+                                <a href="#"><span></span>Admissions</a><div class="menu_arrow2" onclick="myFunction_admissions()"><img src="../../assets_web/images/menu_arrow2.png"></div>
+                                <div class="navigation_dropdown_box admission_dropdown_box" id="admissions_dropdown">
+                                    <div class="width1240">
+                                        <div class="width100">
+                                            <div class="dropdown_list">
+                                                <div class="width100">
+                                                    <ul>
+                                                        <li></li>
+                                                        <li><a href="/pages/about-us/MBA.HTML">MBA</a></li>
+                                                        <li><a href="#">MCA</a></li>
+                                                       
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li><a href="#"><span></span>Commitee</a>
+                                <div class="menu_arrow2" onclick="myFunction_admissions()"><img
+                                            src="../../assets_web/images/menu_arrow2.png"></div>
+                                    <div class="navigation_dropdown_box commitee_dropdown_box"
+                                        id="commitee_dropdown">
+                                        <div class="width240">
+                                            <div class="width100">
+                                                <div class="dropdown_list">
+                                                    <div class="width100">
+                                                        <ul>
+                                                            
+                                                            <li><a href="#">Anti Ragging Commitee </a></li>
+                                                            <li><a href="#">Grievance Redressal Commitee </a></li>
+                                                            <li><a href="#">SC/ST Commitee </a></li>
+                                                            <li><a href="#">Internal Quality Assurance Commitee </a></li>
+                                                            <li><a href="#">Equal Opportunity Commitee </a></li>
+                                                            <li><a href="#">Establishment & Online Grievance Redressal Meachanism</a></li>
+                                                            <li><a href="#">Industry Institude Interaction Cell</a></li>
+                                                           
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li><a href="#">Quick Links  </a>
+                                    <div class="menu_arrow2" onclick="myFunction_admissions()"><img
+                                                src="../pcimca/assets/images/menu_arrow2.png"></div>
+                                        <div class="navigation_dropdown_box quick_links_dropdown_box"
+                                            id="quick_links_dropdown">
+                                            <div class="width240">
+                                                <div class="width100">
+                                                    <div class="dropdown_list">
+                                                        
+                                                        <div class="careers_dropdown_box">
+                                                            <ul>
+                                                                <li><a href="#">AICTE Approval </a></li>
+                                                                <li><a href="#">MHRD</a></li>
+                                                                <li><a href="#">SWAYAM</a></li>
+                                                                <li><a href="#">AICTE Internship</a></li>
+                                                                <li><a href="#">AICTE Teacher Training</a></li>
+                                                                <li><a href="#">Skill development and Enterpreneurship</a></li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                            
+                            <li><a href="#"><span></span>Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
